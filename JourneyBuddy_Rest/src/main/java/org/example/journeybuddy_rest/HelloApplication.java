13c85a8f89fb82431a09bfcb8225b36573c4e387
Moritz Lechthaler <mo.lechthaler@gmx.at>
2024-07-21 22:59:03 +0200

    create Rest

---
+++ JourneyBuddy_Rest/src/main/java/org/example/journeybuddy_rest/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.journeybuddy_rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
