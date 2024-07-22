865b61880b95cd4a2523cc30244807f33bdc68e3
Moritz Lechthaler <mo.lechthaler@gmx.at>
2024-07-22 22:33:00 +0200

    rest model

---
+++ journeybuddy/src/main/java/com/example/journeybuddy/controller/TicketController.java
@@ -0,0 +1,2 @@
+package com.example.journeybuddy.controller;public class TicketController {
+}
---
+++ journeybuddy/src/main/java/com/example/journeybuddy/model/Language.java
@@ -0,0 +1,7 @@
+package com.example.journeybuddy;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Language {
+  }
