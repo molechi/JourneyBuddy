43e006f5ec92f0db764698a37d93081a5cc96b0c
Moritz Lechthaler <mo.lechthaler@gmx.at>
2024-07-22 18:31:55 +0200

    creating  springboot rest

---
+++ journeybuddy/src/main/java/com/example/journeybuddy/JourneyBuddyRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.journeybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyBuddyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyBuddyRestApplication.class, args);
+	}
+
+}
---
+++ journeybuddy/src/test/java/com/example/journeybuddy/JourneyBuddyRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.journeybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyBuddyRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
